9176c4432a37efa6864d9873147aa595e9c47701
janoskyoneill <alejandro.lorca37@gmail.com>
2024-01-24 11:48:33 -0300

    form cel

--- form1.js
+++ form1.js
@@ -4,7 +4,6 @@ function sendMail() {
         correo: document.getElementById("correo").value,
         ciudad: document.getElementById("ciudad").value,
         comuna: document.getElementById("comuna").value,
-        codigoPais: document.getElementById("codigoPais").value,
         telefono: document.getElementById("telefono").value,
         rut: document.getElementById("rut").value,
         ayuda: document.getElementById("ayuda").value,
@@ -20,7 +19,6 @@ function sendMail() {
             document.getElementById("correo").value = "";
             document.getElementById("ciudad").value = "";
             document.getElementById("comuna").value = "";
-            document.getElementById("codigoPais").value = "";
             document.getElementById("telefono").value = "";
             document.getElementById("rut").value = "";
             document.getElementById("ayuda").value = "";
